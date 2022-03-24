1301d4e9764c51ef0bda245b6beedfd2934261c6
Banel <banelbucknor@gmail.com>
2022-03-23 21:17:13 -0400

    tried to fix broken image

--- script.js
+++ script.js
@@ -2,10 +2,33 @@ const button = document.querySelector('button')
 const patternInput = document.querySelector('input')
 const imageDiv = document.querySelector('div')
 
+const emoImg = document.querySelector('.emoImg')
+
 button.addEventListener('click', async () => {
     let response = await axios.get("https://emojihub.herokuapp.com/api/random")
     console.log(response.data)
 
-    let colourPat = response.data[0].url      
-    imageDiv.innerHTML = `<img src=${colourPat}>`     
+    let emoji = response.data.htmlCode[0]    
+    emoImg.innerText = emoji
+
 })
+
+
+
+///////////// TO DO: emoji api page had the code below to add figure out how to make it work. maybe create a json file.
+// import request = require('request');
+
+// const URL = "https://emojihub.herokuapp.com/api/random";
+// request({
+
+//     name: "hugging face",
+//     category: "smileys and people",
+//     group: "face positive",
+//     htmlCode: [
+//       "&#129303;"
+//     ],
+//     unicode: [
+//       "U+1F917"
+//     ]
+//     console.log(data);
+//   }
